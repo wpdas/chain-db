b42d48683ff5adbdc852ecdc7095aa078839c8ca
Wenderson Pires <wendersonpdas@gmail.com>
2025-03-03 17:49:10 -0300

    tests fixed

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,8 @@
 #[cfg(test)]
 mod tests {
+    use std::thread;
+    use std::time::Duration;
+
     use crate::api::models::TableData;
     use crate::chaindb::ChainDB;
 
@@ -16,44 +19,53 @@ mod tests {
         std::fs::remove_dir_all(".chain_db_data/test_db").unwrap();
     }
 
-    // #[test]
-    // fn test_table_operations() {
-    //     // Cria e conecta ao banco
-    //     let _ = ChainDB::create_database("test_db", "test_user", "test_pass");
-    //     let connection = ChainDB::connect("test_db", "test_user", "test_pass").unwrap();
+    #[test]
+    fn test_table_operations() {
+        thread::sleep(Duration::from_secs(2)); // Wait for 2 seconds to ensure the database folder was cleaned
 
-    //     // Cria uma tabela
-    //     let mut table = connection
-    //         .db
-    //         .create_table::<TableData>("test_table")
-    //         .unwrap();
+        // Cria e conecta ao banco
+        let _ = ChainDB::create_database("test_db", "test_user", "test_pass");
+        let connection = ChainDB::connect("test_db", "test_user", "test_pass").unwrap();
 
-    //     // Testa persist
-    //     let mut data = TableData::new();
-    //     data.data
-    //         .insert("test_key".to_string(), serde_json::json!("test_value"));
-    //     assert!(table.persist(&data).is_ok());
+        // Cria uma tabela
+        let mut table = connection
+            .db
+            .create_table::<TableData>("test_table")
+            .unwrap();
 
-    //     // Testa get_table (último registro)
-    //     let result = table.get_table().unwrap();
-    //     assert_eq!(result.data.get("test_key").unwrap(), "test_value");
+        // Testa persist
+        let mut table_data = TableData::new();
+        table_data
+            .data
+            .insert("test_key".to_string(), serde_json::json!("test_value"));
+        assert!(table.persist(&table_data).is_ok());
 
-    //     // Testa update
-    //     let mut new_data = TableData::new();
-    //     new_data
-    //         .data
-    //         .insert("test_key".to_string(), serde_json::json!("updated_value"));
-    //     assert!(table.update(&new_data).is_ok());
+        table_data
+            .data
+            .insert("test_key".to_string(), serde_json::json!("test_value_2"));
+        assert!(table.persist(&table_data).is_ok());
 
-    //     // Testa get_history
-    //     let history = table.get_history(50).unwrap();
-    //     assert_eq!(history.len(), 2);
-    //     // O registro mais recente (updated_value) deve estar no índice 0
-    //     assert_eq!(history[0].data.get("test_key").unwrap(), "updated_value");
-    //     // O registro mais antigo (test_value) deve estar no índice 1
-    //     assert_eq!(history[1].data.get("test_key").unwrap(), "test_value");
+        // Testa get_table (último registro)
+        let result = table.get_table().unwrap();
+        assert_eq!(result.data.get("test_key").unwrap(), "test_value_2");
 
-    //     // Limpa os arquivos de teste
-    //     std::fs::remove_dir_all(".chain_db_data/test_db").unwrap();
-    // }
+        // Testa update
+        let mut new_data = TableData::new();
+        new_data
+            .data
+            .insert("test_key".to_string(), serde_json::json!("updated_value"));
+        assert!(table.update(&new_data).is_ok());
+
+        // Testa get_history
+        let history = table.get_history(50).unwrap();
+        println!("history: {:?}", history);
+        assert_eq!(history.len(), 2);
+        // O registro mais recente (updated_value) deve estar no índice 0
+        assert_eq!(history[0].data.get("test_key").unwrap(), "updated_value");
+        // O registro mais antigo (test_value) deve estar no índice 1
+        assert_eq!(history[1].data.get("test_key").unwrap(), "test_value");
+
+        // Limpa os arquivos de teste
+        std::fs::remove_dir_all(".chain_db_data/test_db").unwrap();
+    }
 }
